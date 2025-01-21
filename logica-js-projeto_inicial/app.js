dac315ae591c3557c470471c8083b0e8f6d0bed1
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-01-21 18:13:22 -0300

    Logic programing - Class 1
    Add the initial project with the first state base of the project for logic programming to stage one

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 100");
+
+if (chute == numeroSecreto) {
+  console.log(`Isso ai! Você descobriu o número secreto (29)!`);
+}
