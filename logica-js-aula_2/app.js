19822a3b7f4acead55c740a123f4fe8fe669b4be
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-01-23 18:10:03 -0300

    Challenges Logic Programing 2 - Questions
    Add the base project to construct the questions proposed in the challenge for learning logic and programming to stage 2

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,11 +1,21 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 8;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 0;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+// enquanto chute for diferente do número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    tentativas++;
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Você acertou o número secreto ${numeroSecreto} em ${tentativas} tentativas!`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
