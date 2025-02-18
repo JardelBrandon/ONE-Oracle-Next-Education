18706f6d1fca0353e940d05e8fbc0ed18f98db6b
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-02-18 17:55:11 -0300

    Challenges 4 - Logic Programing 2 - Answers
    Add challenges resolutions to stage 4 course 2

---
+++ logica-js/logica-js-curso-2/js-curso-2-aula4/logica-js-desafio_4/app.js
@@ -0,0 +1,29 @@
+// Desafio 1
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+// Desafio 2
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 
+// 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+// Desafio 3
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+
+// Desafio 4
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['Ana', 'Maria', 'José'];
+console.log(nomes[0]);
+
+// Desafio 5
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(nomes[1]);
+
+// Desafio 6
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(nomes[nomes.length - 1]);
+
