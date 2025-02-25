3e7a2187c945b80a59c44b94e720e449de113671
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-02-25 20:54:15 -0300

    Add friends
    Add function for insert friends name in the list

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value
+    if (nome == '') {
+        alert('Por favor, insira um nome.')
+        return
+    }
+    if (amigos.includes(nome)) {
+        alert('Nome já adicionado')
+        return
+    }
+    amigos.push(nome)
+    console.log(amigos)
+    document.querySelector('input').value = ''
+}
