a532b4c4411f41dc2a7a7e3365ce7bf9b9e6c10b
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-02-25 21:04:51 -0300

    Update friends
    Add function to update list view of friends for each friend name inserted

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,19 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let amigos = []
+let amigos = [];
 
 function adicionarAmigo() {
-    let nome = document.querySelector('input').value
+    let nome = document.querySelector('input').value;
     if (nome == '') {
-        alert('Por favor, insira um nome.')
-        return
+        alert('Por favor, insira um nome.');
+        return;
     }
     if (amigos.includes(nome)) {
-        alert('Nome já adicionado')
-        return
+        alert('Nome já adicionado');
+        return;
     }
-    amigos.push(nome)
-    console.log(amigos)
-    document.querySelector('input').value = ''
+    amigos.push(nome);
+    console.log(amigos);
+    document.querySelector('input').value = '';
+    atualizarAmigos();
+}
+
+function atualizarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    amigos.forEach(nome => {
+        let item = document.createElement('li');
+        item.textContent = nome;
+        listaAmigos.appendChild(item);
+    })
 }
