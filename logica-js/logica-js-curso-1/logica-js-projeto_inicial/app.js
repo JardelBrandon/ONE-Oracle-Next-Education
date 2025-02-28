cfc824a24f27bf6cf2db686697cfa9a22828d5ff
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-02-28 18:10:47 -0300

    Structure
    Update architecture of project files. Turning the challenge secret friend main into archives on this branch

--- logica-js/logica-js-curso-1/logica-js-projeto_inicial/app.js
+++
@@ -1,7 +0,0 @@
-alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 29;
-let chute = prompt("Escolha um número entre 1 e 100");
-
-if (chute == numeroSecreto) {
-  console.log(`Isso ai! Você descobriu o número secreto (29)!`);
-}
