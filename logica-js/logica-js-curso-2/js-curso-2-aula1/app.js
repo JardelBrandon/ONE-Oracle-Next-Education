8bf7a965a7d2e4a26e366746cc3e9f9f2fc083d4
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2025-03-19 02:05:39 -0300

    Vercel
    Prepare files for upload to cloud, using Vercel and Github

--- logica-js/logica-js-curso-2/js-curso-2-aula1/app.js
+++
@@ -1,9 +0,0 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Tente adivinhar o número secreto entre 1 e 10.';
-
-function verificarChute() {
-    console.log('Chute');
-}
